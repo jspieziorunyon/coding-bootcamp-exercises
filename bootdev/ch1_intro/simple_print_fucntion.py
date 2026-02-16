6a8af63ac8b820f55b5d764f9bc60ef9cd83a1aa
jspieziorunyon <twoshots02@gmail.com>
2026-02-16 11:55:58 -0600

    Add simple print function example in Python
    Some exercises done in chapter 1 of the Python course

---
+++ bootdev/ch1_intro/simple_print_fucntion.py
@@ -0,0 +1,8 @@
+# #some sambple code from ch 1 of python refresher
+
+#print shopkeeper responses
+print("Ah! Great choices...")
+print("Is there anything else I can help you with?")
+
+# simple math in a print function
+print((250 + 241 + 244 + 255) / 4)
